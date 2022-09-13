5115386def2d7357564d8f9cccfdfd6b7b559d65
Niels van der Horst <niels1107@gmail.com>
2022-09-13 22:47:35 +0200

    add posters js

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,8 +1,26 @@
-console.log(movies);
 
-switch (movieFilter) {
 
-    case 1:
+let x = document.getElementById("moviecover");
 
-    break;
+//ul.setAttribute('id', 'movieList');
+for (let index = 0; index < movies.length; index++) {
+    const innerDiv = document.createElement('div');
+    //innerDiv.setAttribute('id', 'innercontainer'+index);
+    innerDiv.id='innercontainer'+index;
+    const ul = document.createElement ('ul');
+    const li = document.createElement('li');
+    var img = document.createElement('img');
+    img.src=movies[index].poster;
+    document.body.appendChild(img);
+    //li.innerHTML= movies[index].poster;
+    ul.appendChild(li);
+    innerDiv.appendChild(ul);
+    document.body.appendChild(innerDiv);
+
+    
 }
+
+
+
+
+    

8f517bc88df4c835fcc971a177f54505239730c3
Niels van der Horst <niels1107@gmail.com>
2022-09-16 20:30:51 +0200

    switch js

--- js/mainscript.js
+++ js/mainscript.js
@@ -1,26 +1,43 @@
+//let x = document.getElementById("moviecover");
 
-
-let x = document.getElementById("moviecover");
-
-//ul.setAttribute('id', 'movieList');
 for (let index = 0; index < movies.length; index++) {
     const innerDiv = document.createElement('div');
-    //innerDiv.setAttribute('id', 'innercontainer'+index);
     innerDiv.id='innercontainer'+index;
+    innerDiv.classList.add("innerDiv");
     const ul = document.createElement ('ul');
     const li = document.createElement('li');
     var img = document.createElement('img');
     img.src=movies[index].poster;
-    document.body.appendChild(img);
-    //li.innerHTML= movies[index].poster;
+    li.appendChild(img);
     ul.appendChild(li);
-    innerDiv.appendChild(ul);
-    document.body.appendChild(innerDiv);
 
-    
+    innerDiv.appendChild(ul);
+   
+    document.body.appendChild(innerDiv);
 }
 
+function filtermovies() {
+
+    let x = category1, category2;
 
 
+    switch (x) {
+        case 'latest-movies':
+            // colorBG('g');
+            break;
+        case 'avenger-movies':
+            // colorBG('r');
+            break;
+        case 'x-men-movies':
+            colorBG('o');
+            break;
+        case 'princess-movies':
+            colorBG('p');
+            break;
+        case 'Batman-movies':
+            colorBG('h');
+            break;
+        default:
+            console.log("Geen Resultaat");    
 
-    
+};
